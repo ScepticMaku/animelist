e2c9d3e5457761bd07b37d211c9463e561cffd61
ScepticMaku <markjayvincentcortes@gmail.com>
2026-05-29 09:39:06 +0800

    added logo

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+
+const appLogo = require('@/assets/images/GojoList Logo.webp');
 
 export default function Index() {
   return (
@@ -11,9 +13,17 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Animelists Project</Text>
+      <Image style={Styles.logo} source={appLogo} />
+      <Text>GojoList</Text>
       <Button title="Browse Anime" onPress={() => router.navigate("/(auth)/signup")} />
       <Button title="Join Now" onPress={() => router.navigate("/(auth)/signup")} />
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  logo: {
+    width: 200,
+    height: 200
+  }
+});
